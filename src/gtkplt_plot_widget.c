0a782292e7d2c3b463992ab63ecf26875be77a9d
Felix Uhl <Felix.Uhl@emea.nec.com>
2021-08-16 14:19:07 +0200

    prepared separate plotting function

--- src/gtkplt_plot_widget.c
+++ src/gtkplt_plot_widget.c
@@ -1,6 +1,7 @@
 #include <gtk/gtk.h>
 
 #include "gtkplt.h"
+#include "plot.h"
 
 gboolean gtkplt_plot_draw_callback(GtkWidget *widget, cairo_t *cr, gpointer widgetdata);
 
@@ -35,20 +36,9 @@ GtkWidget *gtkplt_plot_new() {
 }
 
 gboolean _gtkplt_plot_on_draw(GtkWidget *widget, cairo_t *cr, gpointer data) {
-   GtkPltPlotData plotdata = *((GtkPltPlotData*) data);
+   GtkPltPlotData *plotdata_ptr = (GtkPltPlotData*) data;
 
-   guint width, height;
-   GdkRGBA color;
-   GtkStyleContext *context;
+   gtkplt_plot(widget, cr, plotdata_ptr);
 
-   context = gtk_widget_get_style_context (widget);
-   width = gtk_widget_get_allocated_width (widget);
-   height = gtk_widget_get_allocated_height (widget);
-   gtk_render_background(context, cr, 0, 0, width, height);
-   cairo_arc (cr, width/2.0, height/2.0, MIN (width, height) / 2.0, 0, 2 * G_PI);
-   gtk_style_context_get_color (context, gtk_style_context_get_state (context), &color);
-   gdk_cairo_set_source_rgba (cr, &color);
-   gdk_cairo_set_source_rgba (cr, &color);
-   cairo_fill (cr);
    return FALSE;
 }
