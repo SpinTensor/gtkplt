851d1c471bd332de5105264b0b0720342346528f
Felix Uhl <Felix.Uhl@emea.nec.com>
2021-08-23 11:19:06 +0200

    added functionality to draw xaxis minor ticks and yaxis major/minor ticks

--- src/axis.c
+++ src/axis.c
@@ -6,7 +6,7 @@
 #include "axis.h"
 
 const double majorticks_width = 3.0;
-const double minorticks_width = 1.0;
+const double minorticks_width = 1.5;
 
 GtkPltPlotAxis *gtkplt_axis_init() {
    GtkPltPlotAxis *axis = (GtkPltPlotAxis*) malloc(sizeof(GtkPltPlotAxis));
@@ -62,25 +62,99 @@ double gtkplt_yaxis_area_ymax(GtkPltPlotData *data) {
    return gtkplt_xaxis_area_ymin(data);
 }
 
-void gtkplt_plot_draw_xaxis_majorticks(cairo_t *cr, GtkPltPlotData *data) {
+
+void gtkplt_plot_draw_yaxis_minorticks(cairo_t *cr, GtkPltPlotData *data,
+                                       double yminpos, double ymaxpos,
+                                       double xmaxpos) {
+   double xminpos = xmaxpos - 1.5*majorticks_width;
+
+   int nticks = data->yaxis->nminortics < 0 ? 0 : data->yaxis->nminortics;
+
+   double tickspacing;
+   tickspacing = ymaxpos - yminpos;
+   tickspacing /= nticks + 1;
+
+   cairo_set_line_width(cr, minorticks_width);
+   for (int itick=1; itick<=nticks; itick++) {
+      double y = yminpos + itick*tickspacing;
+      cairo_move_to(cr, xminpos, y);
+      cairo_line_to(cr, xmaxpos, y);
+   }
+}
+
+void gtkplt_plot_draw_yaxis_majorticks(cairo_t *cr, GtkPltPlotData *data) {
+   
    // minimum is two ticks
-   int nticks = data->xaxis->nmajortics < 2 ? 2 : data->xaxis->nmajortics;
+   int nticks = data->yaxis->nmajortics < 2 ? 2 : data->yaxis->nmajortics;
+
+   double yminpos = gtkplt_yaxis_area_ymin(data);
+   double ymaxpos = gtkplt_yaxis_area_ymax(data);
+   double xmaxpos = gtkplt_yaxis_area_xmax(data);
+   double xminpos = xmaxpos - 3*majorticks_width;
 
    double tickspacing ;
-   tickspacing = gtkplt_xaxis_area_xmax(data) - gtkplt_xaxis_area_xmin(data);
+   tickspacing = ymaxpos - yminpos;
    tickspacing /= nticks - 1;
 
-   double xminpos = gtkplt_xaxis_area_xmin(data);
-   double yminpos = gtkplt_xaxis_area_ymin(data);
-   double ymaxpos = yminpos + 3*majorticks_width;
+   cairo_set_line_width(cr, majorticks_width);
+   cairo_set_line_cap(cr, CAIRO_LINE_CAP_SQUARE);
+   for (int itick=0; itick<nticks; itick++) {
+      double y = yminpos + itick*tickspacing;
+      cairo_move_to(cr, xmaxpos, y);
+      cairo_line_to(cr, xminpos, y);
+   }
+   for (int itick=0; itick<nticks-1; itick++) {
+      double y = yminpos + itick*tickspacing;
+      // minor ticks
+      gtkplt_plot_draw_yaxis_minorticks(cr, data, y, y+tickspacing, xmaxpos);
+   }
+}
 
-   for (int itick=0; itick<= nticks; itick++) {
+void gtkplt_plot_draw_xaxis_minorticks(cairo_t *cr, GtkPltPlotData *data,
+                                       double xminpos, double xmaxpos,
+                                       double yminpos) {
+   double ymaxpos = yminpos + 1.5*majorticks_width;
+
+   int nticks = data->xaxis->nminortics < 0 ? 0 : data->xaxis->nminortics;
+
+   double tickspacing;
+   tickspacing = xmaxpos - xminpos;
+   tickspacing /= nticks + 1;
+
+   cairo_set_line_width(cr, minorticks_width);
+   for (int itick=1; itick<=nticks; itick++) {
       double x = xminpos + itick*tickspacing;
       cairo_move_to(cr, x, yminpos);
       cairo_line_to(cr, x, ymaxpos);
    }
+}
 
-   cairo_stroke(cr);
+void gtkplt_plot_draw_xaxis_majorticks(cairo_t *cr, GtkPltPlotData *data) {
+   
+   // minimum is two ticks
+   int nticks = data->xaxis->nmajortics < 2 ? 2 : data->xaxis->nmajortics;
+
+   double xminpos = gtkplt_xaxis_area_xmin(data);
+   double xmaxpos = gtkplt_xaxis_area_xmax(data);
+   double yminpos = gtkplt_xaxis_area_ymin(data);
+   double ymaxpos = yminpos + 3*majorticks_width;
+
+   double tickspacing ;
+   tickspacing = xmaxpos - xminpos;
+   tickspacing /= nticks - 1;
+
+   cairo_set_line_width(cr, majorticks_width);
+   cairo_set_line_cap(cr, CAIRO_LINE_CAP_SQUARE);
+   for (int itick=0; itick<nticks; itick++) {
+      double x = xminpos + itick*tickspacing;
+      cairo_move_to(cr, x, yminpos);
+      cairo_line_to(cr, x, ymaxpos);
+   }
+   for (int itick=0; itick<nticks-1; itick++) {
+      double x = xminpos + itick*tickspacing;
+      // minor ticks
+      gtkplt_plot_draw_xaxis_minorticks(cr, data, x, x+tickspacing, yminpos);
+   }
 }
 
 void gtkplt_plot_draw_axis(cairo_t *cr, GtkPltPlotData *data) {
@@ -95,9 +169,11 @@ void gtkplt_plot_draw_axis(cairo_t *cr, GtkPltPlotData *data) {
    cairo_line_to(cr, gtkplt_xaxis_area_xmin(data), gtkplt_yaxis_area_ymax(data));
    cairo_close_path(cr);
 
-   cairo_stroke(cr);
 
    // dtaw ticks
    gtkplt_plot_draw_xaxis_majorticks(cr, data);
+   gtkplt_plot_draw_yaxis_majorticks(cr, data);
+
+   cairo_stroke(cr);
 
 }
